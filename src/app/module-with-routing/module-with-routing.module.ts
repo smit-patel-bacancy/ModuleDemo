f01262c2581043710546bcde610dd1c6b58527b6
smit-patel-bacancy <smit.patel@bacancy.com>
2024-05-23 09:50:39 +0530

    components added

--- src/app/module-with-routing/module-with-routing.module.ts
+++ src/app/module-with-routing/module-with-routing.module.ts
@@ -4,11 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ModuleWithRoutingRoutingModule } from './module-with-routing-routing.module';
 import { TestComponent } from './test/test.component';
 import { SharedModule } from '../shared/shared.module';
+import { Test2Component } from './test2/test2.component';
+import { Test3Component } from './test3/test3.component';
 
 
 @NgModule({
   declarations: [
-    TestComponent
+    TestComponent,
+    Test2Component,
+    Test3Component
   ],
   imports: [
     CommonModule,
